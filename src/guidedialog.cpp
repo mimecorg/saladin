f848d0014778c15dbed780133d1413055de53271
mimecorg <mimec@mimec.org>
2014-05-06 16:35:22 +0000

    updated copyright year in file headers
    git-svn-id: svn+ssh://svn.code.sf.net/p/saladin/code/trunk@122 23d4f5c6-785b-48be-a11d-f68fea237ed8

--- src/guidedialog.cpp
+++ src/guidedialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * This file is part of the Saladin program
-* Copyright (C) 2011-2013 Michał Męciński
+* Copyright (C) 2011-2014 Michał Męciński
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
